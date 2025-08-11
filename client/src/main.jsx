129fbadbe645c6c9bc822477054ae2d319ca79d7
Meghanareddy244 <meghanareddys999@gmail.com>
2025-08-11 17:09:53 +0530

    updating main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./redux/Store.js";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-      <App />
-    </BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
